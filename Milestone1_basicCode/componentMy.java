10e0ba42a9601623582f405cc3a7fd3128677bba
Alec Fox <alec.d.fox@gmail.com>
2016-02-16 23:36:50 -0500

    added more acknowledgements and tests

--- Milestone1_basicCode/componentMy.java
+++ Milestone1_basicCode/componentMy.java
@@ -21,20 +21,20 @@ import java.util.*;
 
 public class componentMy implements ComponentBase{
 
-private final int init=0;
-private final int success=1;
-private final int failure=2;
+private final int dup=1;
+private final int invalid=2;
+private final int valid=3;
 private Map<String, Integer> tallyTable = new HashMap<String, Integer>(); 
 private ArrayList<String> voterTable = new ArrayList<String>();
-private String [] candidateIDs = {"14", "5", "10", "2"}; //only temporary until admin submits list
+private String [] candidateList; //only temporary until admin submits list
 
 private int state;
 
 public componentMy(){
-    state=init;
+    state=invalid;
 }
 
-/* just a trivial example */
+/* just a trivial example 
 
 private void doAuthentication(String first,String last,String passwd){ //should be changed for voting system specifics
 
@@ -43,11 +43,12 @@ if (first.equals("xin")&&last.equals("li")&&passwd.equals("pass"))
 else 
     state=failure;
 }
+*/
 
 //After checking that the voter has not voted before, the candidateID will be verified
 //If cID is valid then the vote will be logged in the tallyTable, else failure
 private boolean castVoteAuthentication(String cID){
-	for (String s : candidateIDs){
+	for (String s : candidateList){
 		if (s.equals(cID))
 			return true;
 	}
@@ -58,61 +59,80 @@ private boolean castVoteAuthentication(String cID){
 //If it is their first vote then proceed with authentication of cID, and if valid update voter table.
 private void voterAuthentication(String phone, String cID){
 	if (!voterTable.contains(phone)){
-		state = success;
-		if(castVoteAuthentication(cID))
+		if(castVoteAuthentication(cID)){ //if not duplicate vote and cID is valid
 			voterTable.add(phone);
-		else
-			state = failure;
-		/*System.out.println("Voter Table:");
-		for (int i=0; i<voterTable.size(); i++){
-			System.out.println(voterTable.get(i));
-		}*/
+			int count = tallyTable.get(cID);
+			tallyTable.put(cID, ++count); //increment candidate's votes
+			state = valid;
+		}
+		else{
+			state = invalid;
+		}
 	}	
 	else
-		state = failure;
-	//System.out.println();
+		state = dup;
 }
+
+private void initializeCandidates(String password, String [] list){
+	if (password.equals("pass")){
+		candidateList = new String[list.length];
+		for (int i=0; i<list.length; i++){
+			candidateList[i] = list[i];
+			tallyTable.put(list[i], 0);
+		}
+		state = valid;
+	}
+}
+
 /* function in interface ComponentBase */
 
 //Processes the incoming messages with a key value pairing
 public KeyValueList processMsg(KeyValueList kvList){
     int MsgID=Integer.parseInt(kvList.getValue("MsgID"));
+	KeyValueList kvResult = new KeyValueList();
     if (MsgID == 701){ //cast vote
 		voterAuthentication(kvList.getValue("VoterPhoneNo"), kvList.getValue("CandidateID"));
-		KeyValueList kvResult = new KeyValueList();
 		kvResult.addPair("MsgID", "711");
 		kvResult.addPair("Description", "Authentication Result");
 		
 		switch (state){
-			case success: {
+			case valid: {
 				kvResult.addPair("Authentication", "Vote valid");
 				break;
 			}
-			case failure: {
+			case invalid: {
 				kvResult.addPair("Authentication", "Vote invalid");
 				break;
 			}
+			case dup: {
+				kvResult.addPair("Authentication", "Duplicate vote");
+				break;
+			}
 		}
-		return kvResult;
+		
 	}
-	else{
-		doAuthentication(kvList.getValue("FirstName"),kvList.getValue("LastName"),kvList.getValue("passwd"));
-		KeyValueList kvResult = new KeyValueList();
-		kvResult.addPair("MsgID","1");
-		kvResult.addPair("Description","Authentication Result");
-
-	   switch (state) {
-	   case success: {
-		  kvResult.addPair("Authentication","success");
-		  break;
-	   }
-	  case failure: {
-		 kvResult.addPair("Authentication","failure"); 
-		 break;
-	   }
-	   } 
-	  return kvResult; 
-  }
+	else if (MsgID == 703){ //processes initialization of tally table
+		candidateList = kvList.getValue("CandidateList").split(";");
+		initializeCandidates(kvList.getValue("Passcode"), candidateList);
+		kvResult.addPair("MsgID", "26");
+		kvResult.addPair("Description", "Acknowledgement");
+		switch(state){
+			case valid: {
+				kvResult.addPair("AckMsgID", "23");
+				kvResult.addPair("YesNo", "Yes");
+				kvResult.addPair("Name", "GUI");
+				break;
+			}
+			case invalid: {
+				kvResult.addPair("AckMsgID", "23");
+				kvResult.addPair("YesNo", "No");
+				break;
+			}
+		}
+		
+	}
+	
+  return kvResult;
 }
 
 }
